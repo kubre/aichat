558e3ba87d3fff46f9e227d2ff600dea0def019c
Vaibhav Kubre <v@kubre.in>
2024-04-17 22:14:54 +0530

    feat: Added a textinput and rendering multiple models

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
+
+var appLayout = lipgloss.NewStyle()
 
 type Msg struct {
 	username string
@@ -26,7 +31,8 @@ func (m Msg) Description() string {
 }
 
 type Model struct {
-	chat list.Model
+	chat  list.Model
+	input textinput.Model
 }
 
 func (m Model) Init() tea.Cmd {
@@ -37,15 +43,27 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.chat.SetSize(msg.Width, msg.Height)
+	case tea.KeyMsg:
+		if key.Matches(msg, key.NewBinding(key.WithKeys("enter"))) {
+			items := m.chat.Items()
+			items = append(items, Msg{
+				username: "kubre.in",
+				body:     m.input.Value(),
+			})
+			m.chat.SetItems(items)
+			m.input.SetValue("")
+		}
 	}
 
 	var cmd tea.Cmd
-	m.chat, cmd = m.chat.Update(msg)
+	m.input.Focus()
+	m.input, cmd = m.input.Update(msg)
+	cmd = textinput.Blink
 	return m, cmd
 }
 
 func (m Model) View() string {
-	return m.chat.View()
+	return fmt.Sprintf("%s\n%s", m.chat.View(), m.input.View())
 }
 
 func main() {
@@ -56,8 +74,13 @@ func main() {
 		Msg{username: "Bot", body: "Yes, I'm a LLM AI bot"},
 		Msg{username: "kubre.in", body: "That is cool"},
 	}
-	m := Model{chat: list.New(chat, list.NewDefaultDelegate(), 0, 0)}
+	m := Model{
+		chat:  list.New(chat, list.NewDefaultDelegate(), 0, 0),
+		input: textinput.New(),
+	}
 	m.chat.Title = "AI Chat"
+	m.chat.SetFilteringEnabled(false)
+	m.chat.SetShowHelp(false)
 
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println(err)
